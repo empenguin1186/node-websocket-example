b41e144d44b41b21a9a2d581a5ff2823dbbf17bf
empenguin1186 <emperorpenguint1186@gmail.com>
2022-06-18 18:21:27 +0900

    :+1: 通知API連携機能実装

--- app/index.js
+++ app/index.js
@@ -5,19 +5,31 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+app.use(express.json());
+
+// 初回接続時のルーティング
 app.get('/', (req, res) => {
+  req.body
   res.sendFile(__dirname + '/index.html');
 });
 
+// 通知APIのルーティング
+app.post('/message', (req, res) => {
+  res.send('Got a POST request');
+  io.emit('chat message', 'data: ' + req.body.data);
+})
+
+// WebSocket の処理
 io.on('connection', (socket) => {
+  // 初回接続時にクライアントに送信?
+  // socket.broadcast.emit('chat message', 'hello, world!');
+
+  console.log(socket.id);
+
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
 });
-
-// io.on('connection', (socket) => {
-//   socket.broadcast.emit('hi');
-// });
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
